6383994927d2084fee9ac15cc0d5c1347b034852
jeonghi <jpark0902@kookmin.ac.kr>
2024-10-05 05:45:05 +0900

    test(Domain): 도시 날씨 리포지토리 Mock 구현체 추가

---
+++ OpenWeatherApp/Domain/Repository/CityWeatherRepository+Mock.swift
@@ -0,0 +1,26 @@
+//
+//  CityWeatherRepository+Mock.swift
+//  OpenWeatherApp
+//
+//  Created by 쩡화니 on 10/5/24.
+//
+
+import Foundation
+
+final class CityWeatherRepositoryMock: CityWeatherRepositoryType {
+  
+  // MARK: singleton
+  static let shared: CityWeatherRepositoryType = CityWeatherRepositoryMock()
+  
+  // MARK: constructor
+  private init() {}
+  
+  func fetchCityList(page: Int = 1, pageSize: Int = 20, query: String?) async throws -> [City] {
+    [.dummyData, .dummyData]
+  }
+  
+  func fetchWeather(for location: CLLocation) async throws -> Weather {
+    .dummyData
+  }
+}
+
