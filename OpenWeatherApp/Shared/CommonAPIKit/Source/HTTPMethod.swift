c64484394dd9da91067293df71ac295e1b57c29e
jeonghi <jpark0902@kookmin.ac.kr>
2024-10-05 03:52:12 +0900

    Feat/implenment common api module (#4)
    * feat: 라우터 패턴 추상화
    
    - HTTPMethod 열거형 정의
    - 라우터 패턴 추상화
    
    * feat: 네트워크 클라이언트 추상화 클래스 구현

---
+++ OpenWeatherApp/Shared/CommonAPIKit/Source/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  OpenWeatherApp
+//
+//  Created by 쩡화니 on 10/1/24.
+//
+
+import Foundation
+
+// HTTP 메서드를 정의하는 열거형
+enum HTTPMethod: String {
+  case get = "GET"
+  case post = "POST"
+  case put = "PUT"
+  case delete = "DELETE"
+}
