039cbbdbc44cf04875c1a469bec38594a474b2af
jeonghi <jpark0902@kookmin.ac.kr>
2024-10-05 05:47:19 +0900

    feat(Data): openWeatherMap API Client 구현

---
+++ OpenWeatherApp/Data/Remote/Weather/WeatherAPI.swift
@@ -0,0 +1,51 @@
+//
+//  WeatherRouter.swift
+//  OpenWeatherApp
+//
+//  Created by 쩡화니 on 10/1/24.
+//
+
+import Foundation
+
+enum WeatherAPI {
+  case fetchWeatherInfo(params: GetCityWeatherInfoDTO.Request)
+}
+
+extension WeatherAPI: RouterType {
+  
+  var baseUrl: String {
+    Environment.baseUrl
+  }
+  
+  var path: String {
+    switch self {
+    case .fetchWeatherInfo:
+      return "/data/3.0/onecall"
+    }
+  }
+  
+  var method: HTTPMethod {
+    switch self {
+    case .fetchWeatherInfo:
+      return .get
+    }
+  }
+  
+  var headers: [String : String]? {
+    [:]
+  }
+  
+  var parameters: [String : Any]? {
+    switch self {
+    case .fetchWeatherInfo(let params):
+      var dict = params.asDictionary()
+      dict?["appid"] = Environment.apiKey
+      dict?["lang"] = "kr"
+      return dict
+    }
+  }
+  
+  var body: Data? {
+    nil
+  }
+}
