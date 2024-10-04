67d8aeed51ee82c98673d69f7a74836f93c3a46a
jeonghi <jpark0902@kookmin.ac.kr>
2024-10-05 02:52:57 +0900

    chore(.gitignore): xcode프로젝트 무시 처리 제거

---
+++ OpenWeatherApp/Application/OpenWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherAppApp.swift
+//  OpenWeatherApp
+//
+//  Created by 쩡화니 on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
